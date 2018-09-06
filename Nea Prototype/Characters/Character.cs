0de3eef7fa5a0138a828a9d2e004775201516079
Bradley Eaton <bradleyandrew10@msn.com>
2018-09-06 10:09:13 +0100

    Add project files.

---
+++ Nea Prototype/Characters/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+
+namespace Nea_Prototype.Characters
+{
+    public class Character
+    {
+        protected int weight;
+        protected Image sprite;
+        protected int score;
+
+        /// <summary>
+        /// Checks for collisions
+        /// </summary>
+        /// <param name=""></param>
+        public void Collide(int x, int y)
+        {
+
+        }
+
+        public int GetWeight => weight;
+        public Image GetSprite => sprite;
+
+        public int GetScore
+        {
+            get => score;
+            set => score = value;
+        }
+
+    }
+}
---
+++ Nea Prototype/Characters/PlayerOne.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Nea_Prototype.Characters
+{
+    public class PlayerOne : Character
+    {
+        public PlayerOne()
+        {
+            weight = 1;
+            sprite = new Image();
+        }
+
+        
+    }
+}
