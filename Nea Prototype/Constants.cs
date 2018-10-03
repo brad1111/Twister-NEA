6e63afb1e4f2b059f982faed40b45479e5cb6977
Bradley Eaton <bradleyandrew10@msn.com>
2018-10-03 13:23:27 +0000

    Revert "Higher resolution version."

--- Nea Prototype/Constants.cs
+++ Nea Prototype/Constants.cs
@@ -8,8 +8,8 @@
     {
         public const int GRID_TILES_XY = 20;
         public const int CENTRE_TILE_XY = 5;
-        public const int KEYPRESS_PX_MOVED = 2;
-        public const int GRID_ITEM_WIDTH = 40;
+        public const int KEYPRESS_PX_MOVED = 1;
+        public const int GRID_ITEM_WIDTH = 20;
         public const int GRID_WIDTH = GRID_ITEM_WIDTH * GRID_TILES_XY;
     }
 }
