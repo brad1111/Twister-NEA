459152e2949d684ba90f4cf3aa6b0cd375b1364a
Bradley Eaton <bradleyandrew10@msn.com>
2018-10-15 12:39:11 +0100

    Now has algorithms to find angles needed to open.

--- Nea Prototype/Algorithms/Exiting.cs
+++ Nea Prototype/Algorithms/Exiting.cs
@@ -1,20 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
 using Nea_Prototype.Grid;
 
 namespace Nea_Prototype.Algorithms
 {
     public class Exiting
     {
-        public double FindYInTheVerticalDirection(int yValueRelativeToGrid)
+        private List<double> AnglesToOpenInClockwiseDirection = new List<double>();
+        private List<double> AnglesToCloseInClockwiseDirection = new List<double>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e1">The top of the external exit</param>
+        /// <param name="e2">The bottom of the external exit</param>
+        /// <param name="i1">The top of the internal exit</param>
+        /// <param name="i2">The bottom of the internal exit</param>
+        private void FindAnglesNeededToOpenInternal(int e1, int e2, int i1, int i2)
         {
-            //TODO update previous angle to current angle
-            return yValueRelativeToGrid * Math.Cos(GameGridManager.GetGameGrid().PreviousAngle);
+            //Adds degrees version of angle to array
+            AnglesToOpenInClockwiseDirection.Add(Math.Acos(e1 / i1) * (Math.PI/180) );
+            AnglesToCloseInClockwiseDirection.Add(Math.Acos(e2/i2) * (Math.PI/180));
         }
 
-        public double FindXInTheHorizontalDirection(int xValueRelativeToGrid)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e1">The external top height</param>
+        /// <param name="e2">The length of the exit</param>
+        public void FindAnglesNeededToOpen(int e1, int e2)
         {
-            //TODO update previous angle to current angle and check this is right
-            return xValueRelativeToGrid * Math.Sin(GameGridManager.GetGameGrid().PreviousAngle);
+            foreach (var exitLocation in GameGridManager.GetGameGrid().ExitLocationsViews)
+            {
+                int getTop = (int) Canvas.GetTop(exitLocation);
+                FindAnglesNeededToOpenInternal(e1, e2, getTop, getTop + Constants.GRID_ITEM_WIDTH);
+            }
         }
+
+        //public double FindYInTheVerticalDirection(int yValueRelativeToGrid)
+        //{
+        //    //TODO update previous angle to current angle
+        //    return yValueRelativeToGrid * Math.Cos(GameGridManager.GetGameGrid().PreviousAngle);
+        //}
+
+        //public double FindXInTheHorizontalDirection(int xValueRelativeToGrid)
+        //{
+        //    //TODO update previous angle to current angle and check this is right
+        //    return xValueRelativeToGrid * Math.Sin(GameGridManager.GetGameGrid().PreviousAngle);
+        //}
     }
 }
