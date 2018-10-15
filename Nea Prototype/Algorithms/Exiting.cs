5d15baabae9a4ecd33d4bfbce61733172ace0d31
Bradley Eaton <bradleyandrew10@msn.com>
2018-10-15 11:42:38 +0100

    Potential finding the x and y values.

--- Nea Prototype/Algorithms/Exiting.cs
+++ Nea Prototype/Algorithms/Exiting.cs
@@ -1,7 +1,20 @@
-﻿namespace Nea_Prototype.Algorithms
+﻿using System;
+using Nea_Prototype.Grid;
+
+namespace Nea_Prototype.Algorithms
 {
     public class Exiting
     {
-        
+        public double FindYInTheVerticalDirection(int yValueRelativeToGrid)
+        {
+            //TODO update previous angle to current angle
+            return yValueRelativeToGrid * Math.Cos(GameGridManager.GetGameGrid().PreviousAngle);
+        }
+
+        public double FindXInTheHorizontalDirection(int xValueRelativeToGrid)
+        {
+            //TODO update previous angle to current angle and check this is right
+            return xValueRelativeToGrid * Math.Sin(GameGridManager.GetGameGrid().PreviousAngle);
+        }
     }
 }
