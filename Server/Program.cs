718cf6db9827315e370985cd15b46f3a94f6eb56
Bradley Eaton <bradleyandrew10@msn.com>
2018-11-08 14:14:19 +0000

    No longer uses messy lambda expressions for the threading in the server.

--- Server/Program.cs
+++ Server/Program.cs
@@ -21,6 +21,8 @@ namespace Server
         private readonly int PORT_NO = 26332;
         private readonly string levelLocation = String.Empty;
 
+        private Stack<TcpClient> ClientsStack = new Stack<TcpClient>();
+
         static void Main(string[] args)
         {
             Program p = new Program(ref args);
@@ -61,163 +63,172 @@ namespace Server
 
             this.listener = new TcpListener(IPAddress.Any, PORT_NO);
             //New thread
-            this.listenThread = new Thread(new ThreadStart(() =>
-            {
-                listener.Start();
-                while (true)
-                {
-                    Console.WriteLine("Waiting for a connection");
-                    //Thread blocking call (blocks until client has connected)
-                    TcpClient client = listener.AcceptTcpClient();
-
-                    //create a new thread to handle this new client
-                    Thread clientThread = new Thread(new ThreadStart(() =>
-                    {
-                        TcpClient threadClient = client;
-                        NetworkStream clientStream = threadClient.GetStream();
-
-                        byte[] message = new byte[4096];
-                        int bytesRead = 0;
-
-                        bool mapSent = false;
-                        bool mapDownloaded = false;
-                        bool gameStartedOnThread = false;
-
-                        while (true)
-                        {
-                            bytesRead = 0;
-                            try
-                            {
-                                Console.WriteLine($"Waiting for data from {client.ToString()}");
-                                //blocks thread until client sends message
-                                bytesRead = clientStream.Read(message, 0, 4096);
-                            }
-                            catch (SocketException e)
-                            {
-                                //Socket Error has occured
-                                Console.WriteLine(e);
-                                break;
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e);
-                                throw;
-                            }
-
-                            if (bytesRead == 0)
-                            {
-                                //client disconnected
-                                Console.WriteLine($"{client.ToString()} disconnected");
-                                break;
-                            }
-
-                            //msg recieved
-                            ASCIIEncoding encoder = new ASCIIEncoding();
-
-                            string bufferMessage = encoder.GetString(message, 0, bytesRead);
-                            Console.WriteLine(bufferMessage);
-
-                            int characterNo;
-                            //If the map has downloaded
-                            if (gameStartedOnThread)
-                            {
-                                string[] messageSections = bufferMessage.Split(',');
-                                try
-                                {
-                                    characterNo = int.Parse(messageSections[0]);
-                                    double xCoord = double.Parse(messageSections[1]);
-                                    double yCoord = double.Parse(messageSections[2]);
-
-                                    switch (characterNo)
-                                    {
-                                        case 1:
-                                            ServerDataManager.Instance.character1.CharacterPosition.x = xCoord;
-                                            ServerDataManager.Instance.character1.CharacterPosition.y = yCoord;
-                                            break;
-                                        case 2:
-                                            ServerDataManager.Instance.character2.CharacterPosition.x = xCoord;
-                                            ServerDataManager.Instance.character2.CharacterPosition.y = yCoord;
-                                            break;
-                                        default:
-                                            throw new Exception("Too many people have joined.");
-                                    }
-
-                                    ServerChecks();
-                                }
-                                catch (IndexOutOfRangeException e)
-                                {
-                                    Console.WriteLine(
-                                        $"Incorrect format message received: {e.GetType().FullName} {e.Message}");
-                                    continue;
-                                }
-                                catch (FormatException e)
-                                {
-                                    Console.WriteLine(
-                                        $"Incorrect format message received: {e.GetType().FullName} {e.Message}");
-                                    continue;
-                                }
-
-                                ServerChecks();
-
-                                //Converts character 1 to character 2 and vice versa
-                                int otherCharacterNumber = characterNo == 1 ? 2 : 1;
-                                double otherCharacterX = (otherCharacterNumber == 1
-                                    ? ServerDataManager.Instance.character1.CharacterPosition.x
-                                    : ServerDataManager.Instance.character2.CharacterPosition.x);
-                                double otherCharacterY = (otherCharacterNumber == 1
-                                    ? ServerDataManager.Instance.character1.CharacterPosition.y
-                                    : ServerDataManager.Instance.character2.CharacterPosition.y);
-                                //Convert exit array into string
-                                string exits = String.Empty;
-                                foreach (var openExit in ServerDataManager.Instance.ExitsOpen)
-                                {
-                                    exits += openExit.ToString() + ",";
-                                }
-
-                                byte[] buffer = encoder.GetBytes(
-                                    $"{otherCharacterNumber},{otherCharacterX},{otherCharacterY},{ServerDataManager.Instance.CharactersCollided},{ServerDataManager.Instance.CharactersWon},{exits}");
-                                clientStream.Write(buffer, 0, buffer.Length);
-                                clientStream.Flush();
-                                continue;
-                            }
-                            else if (!mapSent)
-                            {
-                                //Send map over (would be in JSON)
-                                byte[] buffer = encoder.GetBytes(ServerDataManager.Instance.levelJson);
-                                clientStream.Write(buffer, 0, buffer.Length);
-                                clientStream.Flush();
-                                mapSent = true;
-                                continue;
-                            }
-                            else if (mapSent && !mapDownloaded)
-                            {
-                                //Check to see if the client says they have recieved it
-                                if (bufferMessage == "received")
-                                {
-                                    //If they have received the map then map is downloaded
-                                    mapDownloaded = true;
-                                    ServerDataManager.Instance.CharacterReady();
-                                }
-                                else if (bufferMessage == "resend")
-                                {
-                                    //The have failed to receive the message send again.
-                                    mapSent = false;
-                                }
-                            }
-                            else if (mapSent && mapDownloaded && ServerDataManager.Instance.GameStarted)
-                            {
-                                //If the game has started overall, tell the client and start the game on this thread
-                                gameStartedOnThread = true;
-                                byte[] buffer = encoder.GetBytes("start");
-                                clientStream.Write(buffer, 0, buffer.Length);
-                                clientStream.Flush();
-                            }
-                        }
-                    }));
-                    clientThread.Start();
-                }
-            }));
+            this.listenThread = new Thread(new ThreadStart(ClientConnection));
             listenThread.Start();
+        }
+        /// <summary>
+        /// Goes through and checks for new clients to connect
+        /// </summary>
+        private void ClientConnection()
+        {
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection");
+                //Thread blocking call (blocks until client has connected)
+                ClientsStack.Push(listener.AcceptTcpClient());
+
+                //create a new thread to handle this new client
+                Thread clientThread = new Thread(new ThreadStart(ClientCommunication));
+                clientThread.Start();
+            }
+        }
+
+        /// <summary>
+        /// Sends/receives messages to/from the client.
+        /// </summary>
+        private void ClientCommunication()
+        {
+            TcpClient threadClient = ClientsStack.Peek();
+            NetworkStream clientStream = threadClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead = 0;
+
+            bool mapSent = false;
+            bool mapDownloaded = false;
+            bool gameStartedOnThread = false;
+
+            while (true)
+            {
+                bytesRead = 0;
+                try
+                {
+                    Console.WriteLine($"Waiting for data from {threadClient.ToString()}");
+                    //blocks thread until client sends message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch (SocketException e)
+                {
+                    //Socket Error has occured
+                    Console.WriteLine(e);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //client disconnected
+                    Console.WriteLine($"{threadClient.ToString()} disconnected");
+                    break;
+                }
+
+                //msg recieved
+                ASCIIEncoding encoder = new ASCIIEncoding();
+
+                string bufferMessage = encoder.GetString(message, 0, bytesRead);
+                Console.WriteLine(bufferMessage);
+
+                int characterNo;
+                //If the map has downloaded
+                if (gameStartedOnThread)
+                {
+                    string[] messageSections = bufferMessage.Split(',');
+                    try
+                    {
+                        characterNo = int.Parse(messageSections[0]);
+                        double xCoord = double.Parse(messageSections[1]);
+                        double yCoord = double.Parse(messageSections[2]);
+
+                        switch (characterNo)
+                        {
+                            case 1:
+                                ServerDataManager.Instance.character1.CharacterPosition.x = xCoord;
+                                ServerDataManager.Instance.character1.CharacterPosition.y = yCoord;
+                                break;
+                            case 2:
+                                ServerDataManager.Instance.character2.CharacterPosition.x = xCoord;
+                                ServerDataManager.Instance.character2.CharacterPosition.y = yCoord;
+                                break;
+                            default:
+                                throw new IndexOutOfRangeException(
+                                    "Too many people have joined, or error in transmission");
+                        }
+
+                        ServerChecks();
+                    }
+                    catch (IndexOutOfRangeException e)
+                    {
+                        Console.WriteLine($"Incorrect format message received: {e.GetType().FullName} {e.Message}");
+                        continue;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine($"Incorrect format message received: {e.GetType().FullName} {e.Message}");
+                        continue;
+                    }
+
+                    ServerChecks();
+
+                    //Converts character 1 to character 2 and vice versa
+                    int otherCharacterNumber = characterNo == 1 ? 2 : 1;
+                    double otherCharacterX = (otherCharacterNumber == 1
+                        ? ServerDataManager.Instance.character1.CharacterPosition.x
+                        : ServerDataManager.Instance.character2.CharacterPosition.x);
+                    double otherCharacterY = (otherCharacterNumber == 1
+                        ? ServerDataManager.Instance.character1.CharacterPosition.y
+                        : ServerDataManager.Instance.character2.CharacterPosition.y);
+                    //Convert exit array into string
+                    string exits = String.Empty;
+                    foreach (var openExit in ServerDataManager.Instance.ExitsOpen)
+                    {
+                        exits += openExit.ToString() + ",";
+                    }
+
+                    byte[] buffer =
+                        encoder.GetBytes(
+                            $"{otherCharacterNumber},{otherCharacterX},{otherCharacterY},{ServerDataManager.Instance.CharactersCollided},{ServerDataManager.Instance.CharactersWon},{exits}");
+                    clientStream.Write(buffer, 0, buffer.Length);
+                    clientStream.Flush();
+                    continue;
+                }
+                else if (!mapSent)
+                {
+                    //Send map over (would be in JSON)
+                    byte[] buffer = encoder.GetBytes(ServerDataManager.Instance.levelJson);
+                    clientStream.Write(buffer, 0, buffer.Length);
+                    clientStream.Flush();
+                    mapSent = true;
+                    continue;
+                }
+                else if (mapSent && !mapDownloaded)
+                {
+                    //Check to see if the client says they have recieved it
+                    if (bufferMessage == "received")
+                    {
+                        //If they have received the map then map is downloaded
+                        mapDownloaded = true;
+                        ServerDataManager.Instance.CharacterReady();
+                    }
+                    else if (bufferMessage == "resend")
+                    {
+                        //The have failed to receive the message send again.
+                        mapSent = false;
+                    }
+                }
+                else if (mapSent && mapDownloaded && ServerDataManager.Instance.GameStarted)
+                {
+                    //If the game has started overall, tell the client and start the game on this thread
+                    gameStartedOnThread = true;
+                    byte[] buffer = encoder.GetBytes("start");
+                    clientStream.Write(buffer, 0, buffer.Length);
+                    clientStream.Flush();
+                }
+            }
         }
 
 
