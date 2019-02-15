edba497573eeee8cbf28ab93f608582dd46dfaa3
Bradley Eaton <bradleyandrew10@msn.com>
2019-02-15 14:40:59 +0000

    Improve rotation delta code.

--- Common/Algorithms/Rotation.cs
+++ Common/Algorithms/Rotation.cs
@@ -25,7 +25,6 @@ namespace Common.Algorithms
                 //Sum the turning moments
                 totalMomentFromCentre += moment;
             }
-
             if (totalMomentFromCentre == 0)
             {
                 //If there's no moment, don't try to divide by 0
@@ -39,12 +38,12 @@ namespace Common.Algorithms
         }
 
         /// <summary>
-        /// 
+        /// Gets the difference in angle between the time now and from a point of time in the future.
         /// </summary>
-        /// <param name="charactersPos"></param>
-        /// <param name="time"></param>
-        /// <param name="weights"></param>
-        /// <returns></returns>
+        /// <param name="charactersPos">The array of character positions</param>
+        /// <param name="time">The time between now and when you want to work out the angle for</param>
+        /// <param name="weights">The array of weights of the characters</param>
+        /// <returns>The difference in angle</returns>
         public static double AbsAngleDelta(Position[] charactersPos, double time, double[] weights)
         {
             double velocity = 9.8; //assume velocity = acceleration * time = 1, assume acceleration is due to gravity
@@ -52,11 +51,13 @@ namespace Common.Algorithms
 
             for (int i = 0; i < charactersPos.Length; i++)
             {
+                //Work out the x and y components of the radius from centre.
                 //Weights are included to simulate effect of heavier characters causing more rotation
                 double radiusFromCentrex = 200 - charactersPos[i].x * weights[i];
                 double radiusFromCentrey = 200 - charactersPos[i].y * weights[i];
-                //c=sqrt(a^2+b^2) (pythagoras)
+                //c=sqrt(a^2+b^2) (pythagoras to find t)
                 double radiusFromCentre = Math.Sqrt(Math.Pow(radiusFromCentrex, 2) + Math.Pow(radiusFromCentrey, 2));
+                //Sum
                 totalDeltaRadians += (velocity * time) / radiusFromCentre;
             }
 
